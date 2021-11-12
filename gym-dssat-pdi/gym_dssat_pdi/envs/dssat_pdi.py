4522b8829281c788955085adc67c72760006fafe
Emilio J. Padrón González <emilioj@udc.gal>
2021-11-12 13:13:27 +0100

    ADD changes to avoid pdirun to launch run_dssat

--- gym-dssat-pdi/gym_dssat_pdi/envs/dssat_pdi.py
+++ gym-dssat-pdi/gym_dssat_pdi/envs/dssat_pdi.py
@@ -196,8 +196,7 @@ class DssatPdi(gym.Env):
         utils.save_file(saving_path=f'{self._tmp_folder}/fileX.MZX', content=self._fileX)
 
     def _launch_client(self):
-        # pdi_command = f'pdirun valgrind --tool=massif --log-file=memcheck.log {self._run_dssat_location} C fileX.MZX {self.experiment_number}'
-        pdi_command = f'pdirun {self._run_dssat_location} C fileX.MZX {self.experiment_number}'
+        pdi_command = f'{self._run_dssat_location} C fileX.MZX {self.experiment_number}'
         pdi_command = pdi_command.split(' ')
         if self.log_saving_path is not None:
             file_path = self.log_saving_path
@@ -211,7 +210,7 @@ class DssatPdi(gym.Env):
         client_process = subprocess.Popen(pdi_command,
                                           stdout=self._f_out,
                                           stderr=sys.stderr,
-                                          shell=False,
+                                          shell=True,
                                           universal_newlines=True,
                                           cwd=self._tmp_folder,
                                           bufsize=0,
